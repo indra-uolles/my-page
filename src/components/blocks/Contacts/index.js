58479bf395da217458b1480dc84e102ddde6a0b3
indra-uolles <smirnovanatalia2008@gmail.com>
2018-01-16 22:53:21 +0300

    contacts request submit

--- src/components/blocks/Contacts/index.js
+++ src/components/blocks/Contacts/index.js
@@ -13,20 +13,20 @@ export default class Contacts extends Component {
             <section className={classnames('contacts', className)} {...props }>
                 <h2>Контакты</h2>
                 <p>Свяжитесь со мной через <TelegramLink link="https://telegram.me/natalia_dushkina"/> или напишите:</p>
-                <form action="/" method="post" className="contacts-form form">
+                <form action="https://formspree.io/smirnovanatalia2008@gmail.com" method="post" className="contacts-form form">
                     <div className="row">
                         <div className="col-md-3 form-control-wrapper">
-                            <div className="form-group field-greecephrase-name required">
-                                <label className="control-label" for="greecephrase-name">Имя / Никнейм</label>
-                                <input type="text" id="greecephrase-name" className="form-control" name="GreecePhrase[name]" placeholder="Имя" autocomplete="off"/>
+                            <div className="form-group required">
+                                <label className="control-label" for="nickname">Имя / Никнейм</label>
+                                <input type="text" id="nickname" className="form-control" name="nickname" placeholder="Имя" autocomplete="off"/>
                                 <div className="help-block"></div>
                             </div>
                             <div className="icon-success-wrapper"><i className="icon-success"></i></div>
                         </div>
                         <div className="col-md-9 idea-form__email form-control-wrapper">
-                            <div className="form-group field-greecephrase-email">
-                                <label className="control-label" for="greecephrase-email">E-mail</label>
-                                <input type="email" id="greecephrase-email" className="form-control" name="GreecePhrase[email]" placeholder="primer@email.ru" autocomplete="off"/>
+                            <div className="form-group">
+                                <label className="control-label" for="email">E-mail</label>
+                                <input type="email" id="email" className="form-control" name="email" placeholder="primer@email.ru" autocomplete="off"/>
                                 <div className="help-block"></div>
                             </div>
                             <div className="icon-success-wrapper">
@@ -36,9 +36,9 @@ export default class Contacts extends Component {
                     </div>
                     <div className="row">
                         <div className="col-md-12">
-                            <div className="form-group field-greecephrase-body required">
-                                <label className="control-label" for="greecephrase-body">Текст</label>
-                                <textarea id="greecephrase-body" className="form-control" name="GreecePhrase[body]" placeholder="С подвыподвертом" autocomplete="off"></textarea>
+                            <div className="form-group required">
+                                <label className="control-label" for="message">Текст</label>
+                                <textarea id="message" className="form-control" name="message" placeholder="С подвыподвертом" autocomplete="off"></textarea>
                                 <div className="help-block"></div>
                             </div>
                             <div className="icon-success-wrapper">
